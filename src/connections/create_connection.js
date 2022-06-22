e1677d9daa01a87212fc8e06f655ffade2060b6d
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-06-22 13:29:31 +0700

    update pass and port for mac

--- src/connections/create_connection.js
+++ src/connections/create_connection.js
@@ -3,7 +3,11 @@ const pool = mysql.createPool({
     connectionLimit : 10,
     host : 'localhost',
     user : 'root',
+    // password : 'root',
     database : 'personalassessment',
+    // port: 8889,
 })
 
 module.exports = pool;
+
+// password and port needed for mac
