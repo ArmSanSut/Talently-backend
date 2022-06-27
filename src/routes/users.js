df02d9e1992cc77336e41780d4534d2e32c8a982
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-06-27 16:32:42 +0700

    update strength router.post to Database

--- src/routes/users.js
+++ src/routes/users.js
@@ -26,6 +26,23 @@ router.get('/strength', async(req,res) => {
     }
 })
 
-
+router.post('/strength/:id', async(req,res) => {
+    try {
+        console.log("test");
+        const id = req.params.id
+        const {strength_1,strength_2,strength_3,strength_4,strength_5,strength_6,strength_7,strength_8} = req.body
+        const [rows, field] = await pool.query(`INSERT INTO \`strength_answer\`(\`user_id\`, \`strength_1\`, \`strength_2\`, \`strength_3\`, \`strength_4\`, \`strength_5\`, \`strength_6\`, \`strength_7\`, \`strength_8\`) VALUES (?,?,?,?,?,?,?,?,?)`, 
+        [id,strength_1,strength_2,strength_3,strength_4,strength_5,strength_6,strength_7,strength_8])
+        console.log(rows);
+        if (rows.affectedRows === 1) {
+            return res.status(200).json({message: "Successfully"})
+        } 
+        return res.status(400).json({ message: "Something went wrong"})
+    }
+    catch(err) {
+        console.log("ERROR", err);
+        res.send(err);
+    }
+})
 
 module.exports = router;
