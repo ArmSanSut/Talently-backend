c56f56722442571ea0748dfdc7c828389ef9ee78
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-06-30 11:27:57 +0700

    set route get and post for achievement database

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,8 @@
 const pool = require('../connections/create_connection');
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const moment = require('moment-timezone');
+const moment1 = require('moment');
 
 //get 30 questions from database
 router.get('/', async (req, res) => {
@@ -64,7 +66,7 @@ router.post('/strength/:id', async (req, res) => {
     try {
         const id = req.params.id
         const { strength_1, strength_2, strength_3, strength_4, strength_5, strength_6, strength_7, strength_8 } = req.body
-        const [rows, field] = await pool.query(`INSERT INTO \`strength_answer\`(\`user_id\`, \`strength_1\`, \`strength_2\`, \`strength_3\`, \`strength_4\`, \`strength_5\`, \`strength_6\`, \`strength_7\`, \`strength_8\`) VALUES (?,?,?,?,?,?,?,?,?)`,
+        const [rows, field] = await pool.query(`INSERT INTO \`strength_answer\`(\`user_id\`, \`strength_1\`, \`strength_2\`, \`strength_3\`, \`strength_4\`, \`strength_5\`, \`strength_6\`, \`strength_7\`, \`strength_8\`) VALUES (?)`,
             [id, strength_1, strength_2, strength_3, strength_4, strength_5, strength_6, strength_7, strength_8])
         console.log(rows);
         if (rows.affectedRows === 1) {
@@ -77,6 +79,40 @@ router.post('/strength/:id', async (req, res) => {
         res.send(err);
     }
 });
+
+
+//post achievement to  database
+router.post('/achievement/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const { date_start, date_end, title, description } = req.body
+        const [rows,] = await pool.query(`INSERT INTO \`achievements\`(\`user_id\`, \`date_start\`, \`date_end\`, \`title\`, \`description\`) VALUES (?,?,?,?,?)`,
+            [id, date_start, date_end, title, description])
+        console.log(rows);
+        if (rows.affectedRows === 1) {
+            return res.status(200).json({ message: "Successfully" })
+        }
+        return res.status(400).json({ message: "Something went wrong" })
+    }
+    catch (err) {
+        console.log("ERROR", err);
+        res.send(err);
+    }
+});
+
+//get achievement from achievement's database
+router.get('/achievement/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const achievement = await pool.query('SELECT * FROM achievements where id = ?', [id])
+        res.json(achievement[0]);
+    }
+    catch (err) {
+        console.log("ERROR", err);
+        res.send(err);
+    }
+})
+
 
 //register account to DB
 router.post('/register', async (req, res) => {
@@ -96,7 +132,11 @@ router.post('/register', async (req, res) => {
     }
 });
 
-
-
-
 module.exports = router;
+
+// router.post('/testtime/', async (req, res) => {
+//     const time = moment(req.body.date_start).tz("Asia/Bangkok").format("MMM Do YY,h:mm:ss a");
+//     console.log(time);
+//     res.send(time.toUpperCase());
+// }
+// );
