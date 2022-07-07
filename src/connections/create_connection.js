8c8e3e353dc6d241d0932e1d7269f7d82e75757b
Pakornpat Amornlertpanit <pakornpat.a@gmail.com>
2022-07-07 10:33:47 +0700

    Connect Database from localhost to cloud

--- src/connections/create_connection.js
+++ src/connections/create_connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     connectionLimit : 10,
-    host : 'localhost',
-    user : 'root',
-    // password : 'root',
+    host : 'ls-627c904378ee6cbf5e370668a843a6d720b988ae.csk6wbdj0thx.ap-southeast-1.rds.amazonaws.com',
+    user : 'mintler',
+    password : 'Databasemintler',
     database : 'personalassessment',
-    // port: 8889,
+    port: 3306,
 })
 
 module.exports = pool;
